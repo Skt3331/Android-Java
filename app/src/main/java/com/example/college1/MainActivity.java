cef0fb6800c89a2f0fe59fa9ec0c56b81d03d331
Skt3331 <sauravkailastemgire@mitacsc.edu.in>
2024-07-15 19:45:54 +0530

    Implimented button for each assignment and added forwordded to that activity

--- app/src/main/java/com/example/college1/MainActivity.java
+++ app/src/main/java/com/example/college1/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.college1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
+        Button buttonForward = findViewById(R.id.buttonForward);
+        buttonForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Hello_world.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        Button ass2 = findViewById(R.id.button2);
+        ass2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Assignment_2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
